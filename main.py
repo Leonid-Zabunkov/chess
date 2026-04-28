c50fcc0742ce09ec5549faa1bf9f45984fbf087c
Max Zabunkov <maksim.zabunkov@myoffice.team>
2026-04-28 23:08:44 +0300

    check made tasks

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from chess_lib.Player import HumanPlayer
 # add_sample_puzzles()
 
 # game = GameStandard()
-game = GamePuzzle.get(2)
 # game = GameFisher()
-timer_w = 500 # int(input("Введите время игры для белых "))
-timer_b = 400 # int(input("Введите время игры для чёрных "))
+game = GamePuzzle.random()
+
+timer_w = 30 # int(input("Введите время игры для белых "))
+timer_b = 30 # int(input("Введите время игры для чёрных "))
 white = HumanPlayer("Max", white=True, timer=timer_w)
 black = HumanPlayer("Leon", white=False, timer=timer_b)
 
