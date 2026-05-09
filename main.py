782b584a5e9429365aca502fd1a82f3b509031af
Leonid Zabunkov <leonidzabunkov@mail.ru>
2026-05-09 18:06:26 +0300

    Task6: adding db and game_puzzle

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+import asyncio
+
 from chess_lib.Game_Standard import GameStandard
 from chess_lib.Game_Fisher import GameFisher
-from chess_lib.Player import Player
+from chess_lib.Game_Puzzle import GamePuzzle
+from chess_lib.Player import HumanPlayer
 
 
-def main(game_mode="standard"):
-    game = GameStandard() if game_mode == "standard" else GameFisher()
+async def main(game_mode, id):
+    match game_mode:
+        case None:
+            game = GameStandard()
+        case "standard":
+            game = GameStandard()
+        case "fisher":
+            game = GameFisher()
+        case "chess960":
+            game = GameFisher()
+        case "puzzle":
+            game = GamePuzzle.random()
+        case "puzzle_id":
+            game = GamePuzzle.get(int(id))
 
-    white = Player("Max", white=True)
-    black = Player("Leon", white=False)
+    white = HumanPlayer("Max", white=True)
+    black = HumanPlayer("Leon", white=False)
 
-    game.start(white, black)
+    await game.start(white, black)
+
+
+# При импорте мейна в другой файл, он не запустится сам, а будет ждать вызова
+if __name__ == "__main__":
+    asyncio.run(main())
